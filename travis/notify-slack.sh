df502f06265008c45e4bc91993b526f633465274
Chris Kalmar <christian.kalmar@gmail.com>
2018-08-15 23:24:36 +0200

    send success or failure message to slack api

--- travis/notify-slack.sh
+++ travis/notify-slack.sh
@@ -10,5 +10,31 @@ COMMIT_HASH=${TRAVIS_COMMIT:0:8}
 BUILD_URL=https://travis-ci.com/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID}
 COMPARE_URL=https://github.com/${TRAVIS_REPO_SLUG}/compare/${TRAVIS_COMMIT_RANGE}
 
-echo "Build <${BUILD_URL}|#${TRAVIS_BUILD_ID}> (<${COMPARE_URL}|${COMMIT_HASH}>) of <b>${TRAVIS_REPO_SLUG}@${TRAVIS_BRANCH}</b> by ${AUTHOR}"
-echo "Commit message: ${TRAVIS_COMMIT_MESSAGE}"
+if [ "$TRAVIS_TEST_RESULT" = "0" ]; then
+  echo
+  echo "Commit message: ${TRAVIS_COMMIT_MESSAGE}"
+
+  read -r -d '' PAYLOAD << EndOfMessage
+  {
+    "attachments": [
+      {
+        "text": "Build <${BUILD_URL}|#${TRAVIS_BUILD_ID}> (<${COMPARE_URL}|${COMMIT_HASH}>) of <b>${TRAVIS_REPO_SLUG}@${TRAVIS_BRANCH}</b> by ${AUTHOR} passed :the_horns:",
+        "color": "good",
+      }
+    ]
+  }
+  EndOfMessage
+else
+  read -r -d '' PAYLOAD << EndOfMessage
+  {
+    "attachments": [
+      {
+        "text": "Build <${BUILD_URL}|#${TRAVIS_BUILD_ID}> (<${COMPARE_URL}|${COMMIT_HASH}>) of <b>${TRAVIS_REPO_SLUG}@${TRAVIS_BRANCH}</b> by ${AUTHOR} failed :sob:",
+        "color": "danger",
+      }
+    ]
+  }
+  EndOfMessage
+fi
+
+curl -X POST -H 'Content-type: application/json' --data "${PAYLOAD}" $SLACK_WEBHOOK_URL
