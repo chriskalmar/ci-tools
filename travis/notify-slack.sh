148164a3e2fe8c6196588d601a6e00b766f8d863
Chris Kalmar <christian.kalmar@gmail.com>
2019-02-06 22:55:14 +0100

    collect messages from all commits within the pushed range for more details

--- travis/notify-slack.sh
+++ travis/notify-slack.sh
@@ -9,9 +9,14 @@ AUTHOR="$(git log -1 $TRAVIS_COMMIT --pretty="%aN")"
 COMMIT_HASH=${TRAVIS_COMMIT:0:8}
 BUILD_URL=https://travis-ci.com/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID}
 COMPARE_URL=https://github.com/${TRAVIS_REPO_SLUG}/compare/${TRAVIS_COMMIT_RANGE}
+TRAVIS_COMMIT_MESSAGE="$(git log $TRAVIS_COMMIT_RANGE --pretty='- *%s*')"
+
+
 POSITIVE_EMOJI=(":the_horns:" ":ok_hand:" ":raised_hands:" ":sunglasses:" ":slightly_smiling_face:" ":relieved:" ":rocket:" ":tada:" ":+1:" ":muscle:" ":balloon:")
 NEGATIVE_EMOJI=(":scream:" ":hankey:" ":bomb:" ":cry:" ":sob:" ":tired_face:" ":face_with_head_bandage:" ":skull_and_crossbones:" ":see_no_evil:" ":biohazard_sign:" ":warning:" ":fire:" ":rain_cloud:" ":radioactive_sign:" ":hurtrealbad:")
 RANDOM=$$$(date +%s)
+
+
 
 if [ "$TRAVIS_TEST_RESULT" = "0" ]; then
   EMOJI=${POSITIVE_EMOJI[$RANDOM % ${#POSITIVE_EMOJI[@]} ]}
