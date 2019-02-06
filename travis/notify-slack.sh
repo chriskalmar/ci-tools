96b89be0cb5ba96c0abb682d9a292d87a2dafa5a
Chris Kalmar <christian.kalmar@gmail.com>
2019-02-06 22:54:29 +0100

    print random emojis for positive and negative builds

--- travis/notify-slack.sh
+++ travis/notify-slack.sh
@@ -9,13 +9,18 @@ AUTHOR="$(git log -1 $TRAVIS_COMMIT --pretty="%aN")"
 COMMIT_HASH=${TRAVIS_COMMIT:0:8}
 BUILD_URL=https://travis-ci.com/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID}
 COMPARE_URL=https://github.com/${TRAVIS_REPO_SLUG}/compare/${TRAVIS_COMMIT_RANGE}
+POSITIVE_EMOJI=(":the_horns:" ":ok_hand:" ":raised_hands:" ":sunglasses:" ":slightly_smiling_face:" ":relieved:" ":rocket:" ":tada:" ":+1:" ":muscle:" ":balloon:")
+NEGATIVE_EMOJI=(":scream:" ":hankey:" ":bomb:" ":cry:" ":sob:" ":tired_face:" ":face_with_head_bandage:" ":skull_and_crossbones:" ":see_no_evil:" ":biohazard_sign:" ":warning:" ":fire:" ":rain_cloud:" ":radioactive_sign:" ":hurtrealbad:")
+RANDOM=$$$(date +%s)
 
 if [ "$TRAVIS_TEST_RESULT" = "0" ]; then
+  EMOJI=${POSITIVE_EMOJI[$RANDOM % ${#POSITIVE_EMOJI[@]} ]}
+
   read -r -d '' PAYLOAD << EndOfSuccess
   {
     "attachments": [
       {
-        "text": "Build <${BUILD_URL}|#${TRAVIS_BUILD_ID}> (<${COMPARE_URL}|${COMMIT_HASH}>) of *${TRAVIS_REPO_SLUG}@${TRAVIS_BRANCH}* \nby ${AUTHOR} passed :the_horns: \nCommit message: _${TRAVIS_COMMIT_MESSAGE}_",
+        "text": ":white_check_mark: \nBuild <${BUILD_URL}|#${TRAVIS_BUILD_ID}> (<${COMPARE_URL}|${COMMIT_HASH}>) of *${TRAVIS_REPO_SLUG}@${TRAVIS_BRANCH}* \nby ${AUTHOR} passed $EMOJI \n${TRAVIS_COMMIT_MESSAGE}",
         "color": "good",
         "fields": [
           {
@@ -34,11 +39,13 @@ if [ "$TRAVIS_TEST_RESULT" = "0" ]; then
   }
 EndOfSuccess
 else
+  EMOJI=${NEGATIVE_EMOJI[$RANDOM % ${#NEGATIVE_EMOJI[@]} ]}
+
   read -r -d '' PAYLOAD << EndOfFailure
   {
     "attachments": [
       {
-        "text": "Build <${BUILD_URL}|#${TRAVIS_BUILD_ID}> (<${COMPARE_URL}|${COMMIT_HASH}>) of *${TRAVIS_REPO_SLUG}@${TRAVIS_BRANCH}* \nby ${AUTHOR} failed :sob: \nCommit message: _${TRAVIS_COMMIT_MESSAGE}_",
+        "text": ":no_entry: \nBuild <${BUILD_URL}|#${TRAVIS_BUILD_ID}> (<${COMPARE_URL}|${COMMIT_HASH}>) of *${TRAVIS_REPO_SLUG}@${TRAVIS_BRANCH}* \nby ${AUTHOR} failed $EMOJI \n${TRAVIS_COMMIT_MESSAGE}",
         "color": "danger",
       }
     ]
